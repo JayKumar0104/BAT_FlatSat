8775b62e605d4e2a7fe361ca019cb731b6f627f3
Jay Kumar <74879395+JayKumar0104@users.noreply.github.com>
2026-01-04 15:32:12 -0800

    Remove unused imports and print statement

--- test.py
+++ test.py
@@ -1,15 +1,2 @@
-import math
-import time
-import board
-from adafruit_lsm6ds.lsm6dsox import LSM6DSOX as LSM6DS
-from adafruit_lis3mdl import LIS3MDL
-from git import Repo
-from picamera2 import Picamera2
-from datetime import datetime
-from git import Repo
-
-accelx, accely, accelz = accel_gyro.acceleration
-a_mag = math.sqrt(accelx**2 + accely**2 + accelz**2)
 while 0 < 1:
-  print(a_mag)
   print("lebron")
