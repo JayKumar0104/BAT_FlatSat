b4e18da73f6aa6df31093edc1f2818e5f7536a4c
Jay Kumar <74879395+JayKumar0104@users.noreply.github.com>
2026-01-04 15:42:45 -0800

    Set up I2C and sensors in test.py
    Initialize I2C and sensor instances for data collection.

--- test.py
+++ test.py
@@ -7,7 +7,14 @@ from git import Repo
 from picamera2 import Picamera2
 from datetime import datetime
 from git import Repo
+
+i2c = board.I2C()
+accel_gyro = LSM6DS(i2c)
+mag = LIS3MDL(i2c)
+picam2 = Picamera2()
+
 print("lebron")
+
 while 67<69:
   accelx, accely, accelz = accel_gyro.acceleration
   a_mag = math.sqrt(accelx**2 + accely**2 + accelz**2)
